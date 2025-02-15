6072aed74e0c4ee34c65e36175ff5bd20c2c9f86
Ashutosh Mathore <ashutoshmathore@csus.edu>
2025-02-15 01:03:09 -0800

    Add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import CouponSearch from "@/components/coupon-search";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
+    <>
     <CouponSearch />
+    <Analytics />
+    </>
   );
 }
